8eb11802e39049aa24ae623665195b4862824337
JessicaWong230142 <113719950+JessicaWong230142@users.noreply.github.com>
2024-02-05 00:20:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from image_processing import image_processing
 #reads img file in full color
 video = cv2.VideoCapture('circle1.MOV')
 
+#runs the image_processing function and breaks out of it if error
 while True:
     image_processing(video)
 
+#makes each frame appear and then destroys
 video.release()
 cv2.destroyAllWindows()
